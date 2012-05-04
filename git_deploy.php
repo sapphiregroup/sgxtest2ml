69be99e13bf0d5194fce2fb89658ae8d415b323d
n <N@n.com>
2012-05-04 13:35:17 +0100

    PP

--- git_deploy.php
+++ git_deploy.php
@@ -9,15 +9,13 @@ catch(Exception $e)
 }
 
 //log the request
-file_put_contents('gitlog.txt', print_r($payload, TRUE), FILE_APPEND);
+// DEBUG
+// file_put_contents('gitlog.txt', print_r($payload, TRUE), FILE_APPEND);
+$timestamp=date("d-m-y-",time());
+file_put_contents('gitlog.txt', print_r("$timestamp - Push initiated by GIT", TRUE), FILE_APPEND);
 
 
 if ($payload->ref === 'refs/heads/master')
 {
-file_put_contents('gitlog.txt', print_r("Inside Payload Correct", TRUE), FILE_APPEND);
-  // path to your site deployment script
-file_put_contents('gitlog.txt', print_r("Exec Deploy Payload", TRUE), FILE_APPEND);
-//  exec('./git_deploy.sh');
 shell_exec('git pull');
-file_put_contents('gitlog.txt', print_r("End Deploy Payload", TRUE), FILE_APPEND);
 }
