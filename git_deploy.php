35189ca7ee4632609ae49cc64a0af5be6c32fbc2
n <N@n.com>
2012-05-04 09:58:23 +0100

    aaaa

--- git_deploy.php
+++ git_deploy.php
@@ -9,11 +9,11 @@ catch(Exception $e)
 }
 
 //log the request
-file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+file_put_contents('gitlog.txt', print_r($payload, TRUE), FILE_APPEND);
 
 
 if ($payload->ref === 'refs/heads/master')
 {
   // path to your site deployment script
-  exec('./build.sh');
+  exec('./git_deploy.sh');
 }
